537cd03c8ac0be88018e924c74d2bb522e4d8044
FarruxDEV <farruxbeknozimov@gmail.com>
2023-03-20 18:29:47 +0500

    Connect to mongodb and create user table

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const start = async () => {
+  try {
+    const app = await NestFactory.create(AppModule);
+    const PORT = process.env.PORT || 7000;
+    app.setGlobalPrefix('api');
+
+    app.listen(PORT, () => {
+      console.log(`Server ${PORT} da yuguryapti...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
